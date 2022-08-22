3fbf35311ff89dbaf46b65c384e77d962615ec70
vodkaJpg <99024195+vodkaJpg@users.noreply.github.com>
2022-08-22 18:59:00 +0200

    refreshing files

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
-const PasswordField = document.querySelector('#PasswordFiled')
-const BntPasswordView = document.querySelector('#bnt-password-view')
+
+var calculateComplexity = function (password) {
+    var complexity = 0;
+    
+    var regExps = [ 
+      /[a-z]/,
+      /[A-Z]/,
+      /[0-9]/,
+      /.{8}/,
+      /.{16}/,
+      /[!-//:-@[-`{-ÿ]/
+    ];
+    
+    regExps.forEach(function (regexp) {
+      if (regexp.test(password)) {
+        complexity++;
+      }
+    });
+    
+    return {
+      value: complexity,
+      max: regExps.length
+    };
+  };
+   
+  var checkPasswordStregth = function (password) {
+    var progress = document.querySelector('#passwordComplexity'),
+        complexity = calculateComplexity(this.value); 
+    
+    progress.value = complexity.value;
+    progress.max = complexity.max;
+  };
+  
+  var input = document.querySelector('#passwordField');
+  input.addEventListener('keyup', checkPasswordStregth);
+  
+  var PasswordField = document.querySelector('#passwordFiled');
+var BntPasswordView = document.querySelector('#bnt-password-view');
 BntPasswordView.addEventListener('click', () => {
-    toggle(PasswordField)
+    toggle(PasswordField);
 });
 
-function toggle(PasswordField){PasswordField.type=(PasswordField.type=="password")?"text":"password";}
+function toggle(e){e.type=(e.type=="password")?"text":"password";}
